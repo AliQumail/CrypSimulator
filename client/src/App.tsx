d69d2edc78c02e7003576c25f9bc8fd6b786a01d
Ali Qumail <aliqumail8@gmail.com>
2024-06-08 15:34:36 +0500

    FEAT - Added Tailwindcss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,6 +11,14 @@ function App() {
           <Route path="/dashboard" element={<Dashboard/>} />
         </Routes>
       </BrowserRouter>
+
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+       </h1>
+       
+<button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Default</button>
+
+
     </div>
   );
 }
