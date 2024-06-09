7b078faaa868d0ca3f819391a3e0d54205e3d6fd
Ali Qumail <aliqumail8@gmail.com>
2024-06-09 17:51:31 +0500

    FEAT - Add a Buy modal

--- client/src/components/dashboard/Dashboard.tsx
+++ client/src/components/dashboard/Dashboard.tsx
@@ -5,6 +5,7 @@ import { formatData } from "../../utils";
 // import { Container, Row, Col } from "react-bootstrap";
 import axios from "axios";
 import HistoryChart from "./components/chart/HistoryChart";
+import BuyModal from "./components/BuyModal";
 // import TotalStats from "./components/TotalStats";
 // import Records from "./components/Records";
 // import CurrencyStats from "./components/CurrencyStats";
@@ -217,6 +218,7 @@ export default function User() {
         <button type="button" onClick={handleBuy} className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
           Green
         </button>
+        <BuyModal/>
         </div>
         <div className="col-span-8">
           <h4>Current Balance: {currBal} </h4>
