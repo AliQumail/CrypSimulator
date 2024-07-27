4a774986553abefe08c469e1ec996f407f7fcaa9
Ali Qumail <aliqumail8@gmail.com>
2024-07-27 12:38:35 +0500

    IMP - Removing Auth Service

--- server/src/AuthService/DTOs/LoginResponseDto.cs
+++
@@ -1,7 +0,0 @@
-namespace DTOs
-{
-    public class LoginResponseDto
-    {
-        public string JwtToken { get; set; } = string.Empty;
-    }
-}
--- server/src/AuthService/Data/AuthDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-namespace AuthService.Data;
-
-public class AuthDbContext: IdentityDbContext
-{
-    public AuthDbContext(DbContextOptions options): base(options) {}
-
-}
