7c38b46e6357bde952d812e074719f3f009c1da6
Ali Qumail <aliqumail8@gmail.com>
2024-08-01 18:07:52 +0500

    FEAT - Implemented search in user portfolio table

--- client/src/components/dashboard/components/showUserPortfolio/ShowUserPortfolio.tsx
+++ client/src/components/dashboard/components/showUserPortfolio/ShowUserPortfolio.tsx
@@ -14,8 +14,30 @@ interface ShowUserPortfolioProps {
 }
 
 const ShowUserPortfolio: FC<ShowUserPortfolioProps> = ({ userPortfolio }) => {
+
+  const [searchValue, setSearchValue] = useState("");
+  const [filteredUserPortfolio, setfilteredUserPortfolio] = useState<IUserPortfolio[]>(userPortfolio);
+
+  useEffect(() => {
+    // Perform filtering whenever searchValue or transactions change
+    const filtered = userPortfolio.filter(userPortfolio =>
+      userPortfolio.currencyName.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    console.log(filtered);
+    setfilteredUserPortfolio(filtered);
+  }, [searchValue, userPortfolio]);
+
   return (
     <div className="relative overflow-x-auto">
+      <div className="search relative flex justify-end overflow-x-auto">
+        <input
+          type="text"
+          placeholder="Search"
+          className="p-2 border-2 border-gray-400 rounded mt-4"
+          value={searchValue}
+          onChange={e => setSearchValue(e.target.value)}
+        />
+      </div>
       <table className="w-full text-lg text-left rtl:text-right text-black-500 dark:text-black-400 mt-5">
         <thead className="text-lg text-black-900 uppercase dark:text-black-400 border-b-2 border-gray-200">
            <tr>
@@ -25,8 +47,8 @@ const ShowUserPortfolio: FC<ShowUserPortfolioProps> = ({ userPortfolio }) => {
         </thead>
         <br/>
         <tbody>
-          {userPortfolio && userPortfolio.length > 0 ? (
-            userPortfolio.filter(tran=> tran.currencyName !== "USD").map((tran) => (
+          {filteredUserPortfolio && filteredUserPortfolio.length > 0 ? (
+            filteredUserPortfolio.filter(port => port.currencyName !== "USD").map((tran) => (
               <tr key={tran.id}> {/* Unique key for each row */}
                 <td>{tran.currencyName}</td>
                 <td>{tran.quantity}</td>
